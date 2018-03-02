799dd99e8f32c65e10b46b945c0830ddc71baf4b
Bruce <baixin137@gmail.com>
2018-03-01 23:36:03 -0600

    partition now runs correctly

--- src/HodorFS/BufferManager.cpp
+++ src/HodorFS/BufferManager.cpp
@@ -598,6 +598,7 @@ Page* BufferManager::get(int pn) {
 
 
 void BufferManager::MoveTuple(PageSet* pnew, PageSet* pold, size_t line) {
+	pnew->slots -= 1;
 	for (size_t i = 0; i < pold->pageset.size(); i++) {
 		Page* page_old = get(pold->pageset[i]);
 		Page* page_new = get(pnew->pageset[i]);
